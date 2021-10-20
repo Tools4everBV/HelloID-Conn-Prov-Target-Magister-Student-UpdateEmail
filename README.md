ddd7bd6869dddd24d5b4cac79db88c149d122d7f
pdoelwijt <p.doelwijt@tools4ever.com>
2021-10-20 15:21:22 +0200

    v1

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# HelloID-Conn-Prov-Target-Magister
+# HelloID-Conn-Prov-Target-magister
+
+
+
+## Table of contents
+
+- [Introduction](#Introduction)
+- [Getting started](#Getting-started)
+  + [Connection settings](#Connection-settings)
+  + [Prerequisites](#Prerequisites)
+  + [Remarks](#Remarks)
+- [Setup the connector](Setup-The-Connector)
+- [Getting help](Getting-help)
+
+## Introduction
+The HelloID-Conn-Prov-Target-magister can be used update user accounts in Magister. You can update for example the email addres and password.
+
+## Getting started
+
+### Prerequisites
+- URL to the webservice. Example: https://tools4ever.swp.nl:8800
+ - username
+ - password
+ - library
+ - function
+
+### Actions
+|Library| function     | Description |
+| ------------| ------------ | ----------- |
+| ADFuncties     | UpdatePersEMail  | update mailadres of employee|
+| ADFuncties     | UpdateLeerEMail  | update mailadres of student|
+| Data     | Set.Password  | set password|
+
+
+
+
+### Connection settings
+The following settings are required to connect to the API.
+
+
+| Setting     | Description |
+| ------------ | ----------- |
+| username     | The username   |
+| Password   | The password  |
+| BaseUrl    |    The URL to the Magister environment. Example: https://tools4ever.swp.nl:8800|
+| Library    |  Name of the library off the function |
+| Function   | Name of the function to execute |
+
+
+### Remarks
+  - Execute on-premises
+  - The user must be authorized for the function 
+  - Documentation can be found at https://<tenant>.swp.nl:8800/doc?, https://<tenant>.nl:8800/doc?#Service_ADFuncties, https://<tenant>.swp.nl:8800/doc?#Service_Data 
+ 
+
+## Getting help
+
+> _For more information on how to configure a HelloID PowerShell connector, please refer to our [documentation](https://docs.helloid.com/hc/en-us/articles/360012557600-Configure-a-custom-PowerShell-source-system) pages_
+
+> _If you need help, feel free to ask questions on our [forum](https://forum.helloid.com)_
+
+## HelloID Docs
+
+The official HelloID documentation can be found at: https://docs.helloid.com/
---
+++ create-employee-email.ps1
@@ -0,0 +1,102 @@
+#Config
+$c = $configuration | ConvertFrom-Json
+$user = $c.user;
+$magisterBaseUri = "";
+$pass = $c.pass;
+$tenant = $c.tenant
+#$magisterFunction = "UpdatePersEMail";
+$magisterFunction=c.function;
+#$magisterLibrary = "ADFuncties";
+$magisterLibrary = "$c.library;
+$magisterUsername = $c.user;
+$magisterPassword = $pass;
+
+
+#Enable TLS 1.2
+if ([Net.ServicePointManager]::SecurityProtocol -notmatch "Tls12") {
+    [Net.ServicePointManager]::SecurityProtocol += [Net.SecurityProtocolType]::Tls12
+}
+
+#Initialize default properties
+$p = $person | ConvertFrom-Json;
+$stamNr = $p.ExternalId;
+$aRef = $p.ExternalId;
+$success = $False;
+$auditMessage = "";
+
+#Read UPN from AD,change to the correct source
+$userPrincipalName = $p.Accounts.MicrosoftActiveDirectory.userPrincipalName;
+
+#Change Magister Base URI based on location code
+$locationCode = $p.PrimaryContract.Location.Code;
+$magisterBaseUri = "https://$tenant.swp.nl:8800";
+
+if ($magisterBaseUri -eq "nvt"  ) {
+    $auditMessage = "User is not in Magister, skipping update..."
+    $success = $False;
+
+    #build up result
+    $result = [PSCustomObject]@{
+        Success          = $success;
+        AccountReference = $aRef;
+        AuditDetails     = $auditMessage;
+        Account          = $account;
+    };
+
+    Write-Output $result | ConvertTo-Json -Depth 10;
+    exit
+}
+
+#Create account object
+$account = [PSCustomObject]@{
+    'ref'   = $p.ExternalId
+    'email' = $userPrincipalName
+    'locationCode' = $locationCode
+}
+
+if ([String]::IsNullOrEmpty($userPrincipalName)  ) {
+    $auditMessage = "AD UPN empty, skipping update..."
+    $success = $False;
+
+    #build up result
+    $result = [PSCustomObject]@{
+        Success          = $success;
+        AccountReference = $aRef;
+        AuditDetails     = $auditMessage;
+        Account          = $account;
+    };
+
+    Write-Output $result | ConvertTo-Json -Depth 10;
+    exit
+}
+
+# Compose update uri
+$uri = "$magisterBaseUri/doc?Function=$magisterFunction&Library=$magisterLibrary&SessionToken=$magisterUsername%3B$magisterPassword&StamNr=$stamNr&EMail=$userPrincipalName";
+
+Write-Verbose $uri -Verbose
+
+try {
+    if (-Not($dryRun -eq $True)) {
+        $response = Invoke-WebRequest -Method POST -Uri $uri -UseBasicParsing
+        if ($response.statuscode -eq "200") {
+            $success = $True;
+            $auditMessage = "Email address updated";
+        } else {
+            $success = $False;
+            $auditMessage = "Update failed: $reponse";
+        }
+    }
+}
+catch {
+    $errResponse = $_;
+    $auditMessage = "Update failed: ${errResponse}";
+}
+#build up result
+$result = [PSCustomObject]@{
+    Success          = $success;
+    AccountReference = $aRef;
+    AuditDetails     = $auditMessage;
+    Account          = $account;
+};
+
+Write-Output $result | ConvertTo-Json -Depth 10;
